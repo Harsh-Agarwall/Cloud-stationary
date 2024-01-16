89fbace38ec0a9269f9bba58997405c3f1508ede
Harsh-Agarwall <harsh.code0@gmail.com>
2024-01-16 23:11:16 +0530

    changes

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const port = 8000;
-const path = require("path");
 const ejsMate = require("ejs-mate");
+require('./db/conn');
+const path = require("path");
+const port = 8000;
+const User=require('./model/userschema');
 app.engine("ejs",ejsMate);
-app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
-app.use(express.urlencoded({extended:true}));
+app.set("view engine","ejs");
 
-const User=require('./model/userschema');
 
 
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 app.use(require('./router/authy'));
 
 app.listen(port, (req, res) => {
