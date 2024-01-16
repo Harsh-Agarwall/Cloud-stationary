4e415fed52e2d1eaed912a3ca4c7193632df5b76
Shubham Singh <singhofficial1611@gmail.com>
2024-01-16 21:58:56 +0530

    shubham

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.engine("ejs",ejsMate);
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:true}));
 
 const User=require('./model/userschema');
 
